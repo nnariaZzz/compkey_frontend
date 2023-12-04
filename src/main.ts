067a7243617c761a6a66c94909553d120d8b401d
nnaria <2665567234@qq.com>
2023-12-05 01:02:16 +0800

    main

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import App from "./App.vue"
 import router from "@/router/router"
 import { createApp } from "vue"
 import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
 app.use(router)
 app.mount("#app")
 app.use(ElementPlus)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
